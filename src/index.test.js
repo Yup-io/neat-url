acd3d7b39f1f786965687d9a8c0abc7e451a7583
Cy <none@none.www>
2019-09-15 22:15:56 -0600

    Adding index and tests

---
+++ src/index.test.js
@@ -0,0 +1,26 @@
+const neatURL = require("./index")
+
+describe('neatURL', () => {  
+
+	it("Remove utm_source from querystring", () => {
+		expect(neatURL({
+			url: "https://www.example.com/bens-tagging?utm_source=mysite.com&utm_medium=referral&utm_campaign=url+tracking+post"
+		})).toBe("https://www.example.com/bens-tagging");
+	});
+
+	it("Remove Echobox hash variable", () => {
+		expect(neatURL({
+			url: "https://nationalpost.com/news/world/in-edward-snowdens-new-memoir-the-disclosures-this-time-are-personal#Echobox=1568564590",
+			includeHash: true
+		})).toBe("https://nationalpost.com/news/world/in-edward-snowdens-new-memoir-the-disclosures-this-time-are-personal");
+	});
+
+	it("Ignore hash key with no value", () => {
+		expect(neatURL({
+			url: "https://nationalpost.com/news/world/in-edward-snowdens-new-memoir-the-disclosures-this-time-are-personal#abcdefg",
+			includeHash: true
+		})).toBe("https://nationalpost.com/news/world/in-edward-snowdens-new-memoir-the-disclosures-this-time-are-personal#abcdefg");
+	});
+
+});
+
